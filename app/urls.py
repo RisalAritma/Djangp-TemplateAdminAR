1c8bd9af42fb360ae1325d4ab32c93b8632460cf
Risal <risal.aritma@gmail.com>
2026-07-10 14:09:31 +0800

    update template awal

--- app/urls.py
+++ app/urls.py
@@ -1,14 +1,11 @@
 from django.urls import path
-from . import views, views_fakultas
+from . import views
 
 app_name = 'app'
 
 urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
 
-
-    # FAKULTAS
-    path('dashboard_fakultas/', views_fakultas.dashboard, name='dashboard_fakultas'),
 
 
 ]
--- app/views.py
+++ app/views.py
@@ -8,15 +8,8 @@ from django.shortcuts import redirect
 def dashboard(request):
     user = request.user
     
-    if user.last_name == 'Admin Fakultas':
-        redirect_url = 'app:dashboard_fakultas'  # Ganti dengan URL yang sesuai untuk dashboard admin fakultas
-    elif user.last_name == 'Admin Jurusan':
-        redirect_url = 'app:dashboard_jurusan'  # Ganti dengan URL yang sesuai untuk dashboard admin jurusan
-    elif user.last_name == 'Dosen':
-        redirect_url = 'app:dashboard_dosen'  # Ganti dengan URL yang sesuai untuk dashboard dosen
-    elif user.last_name == 'Mahasiswa':
-        redirect_url = 'app:dashboard_mhs'  # Ganti dengan URL yang sesuai untuk dashboard mahasiswa
-    else:
-        redirect_url = 'app:dashboard_blank'  # Ganti dengan URL yang sesuai untuk dashboard default atau halaman kosong
-
-    return redirect(redirect_url)   
+    context = {
+        'user': user,
+        'title': 'Dashboard',
+    }
+    return render(request, 'app/dashboard.html', context)
